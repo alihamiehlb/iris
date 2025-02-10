98b296156892545cefca3c2f975c68f028296a25
alihamiehlb <alihamiehlb@gmail.com>
2025-02-10 18:30:30 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
+import gunicorn
+
 
 # Load the trained model
 model = pickle.load(open("iris_model.pkl", "rb"))
